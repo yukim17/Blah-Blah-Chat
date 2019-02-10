077b6e06ae72c3ebd1e5060605e0b338fc66abba
Kate G <kath.grish@gmail.com>
2019-02-10 22:47:35 +0300

    Log App and UIViewController lifecycle

---
+++ Blah Blah Chat/Logger.swift
@@ -0,0 +1,34 @@
+//
+//  Logger.swift
+//  Blah Blah Chat
+//
+//  Created by Екатерина on 10.02.2019.
+//  Copyright © 2019 Wineapp. All rights reserved.
+//
+
+import Foundation
+
+enum ApplicationState: String {
+    case notRunning = "not running"
+    case inactive
+    case active
+    case background
+    case suspended
+}
+
+class Logger {
+    
+    static let shared = Logger()
+    
+    func state(function: String = #function, moveFrom state1: ApplicationState, to state2: ApplicationState) {
+        #if DEBUG
+            print("Application moved from \"\(state1.rawValue)\" to \"\(state2.rawValue)\": \(function)")
+        #endif
+    }
+    
+    func state(function: String = #function, message: String) {
+        #if DEBUG
+            print("View controller calls \(function): \(message)")
+        #endif
+    }
+}
--- Blah Blah Chat/ViewController.swift
+++ Blah Blah Chat/ViewController.swift
@@ -9,10 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let logger = Logger.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        logger.state(message: "View is created and loaded into memory")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        logger.state(message: "The view is about to be presented on screen")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        logger.state(message: "The view is just presented on the screen")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        logger.state(message: "The view is going to set up subviews")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        logger.state(message: "The subviews of the view have been setup")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        logger.state(message: "The view is about to be removed from screen")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        logger.state(message: "The view is just removed from screen")
     }
 
 
