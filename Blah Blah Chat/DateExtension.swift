94fff82d68ed08d7ae1f37a8456dd711febf22e7
Kate G <kath.grish@gmail.com>
2019-02-27 19:11:51 +0300

    bug fix

---
+++ Blah Blah Chat/DateExtension.swift
@@ -0,0 +1,23 @@
+//
+//  DateExtension.swift
+//  Blah Blah Chat
+//
+//  Created by Екатерина on 24.02.2019.
+//  Copyright © 2019 Wineapp. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    
+    func toShortFormatString() -> String {
+        let calendar = Calendar.current
+        let dateFormatter = DateFormatter()
+        if calendar.isDateInToday(self) {
+            dateFormatter.dateFormat = "HH:mm"
+        } else {
+            dateFormatter.dateFormat = "dd MMM"
+        }
+        return dateFormatter.string(from: self)
+    }
+}
