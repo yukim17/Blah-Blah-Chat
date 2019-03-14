7db854e2a06946a6ebe1d5eb1f5639ed84acc66d
Kate G <kath.grish@gmail.com>
2019-03-14 09:39:39 +0300

    added activity indicator & change file structure

--- Blah Blah Chat/UI/Profile/ProfileViewController.swift
+++ Blah Blah Chat/UI/Profile/ProfileViewController.swift
@@ -18,6 +18,7 @@ class ProfileViewController: UIViewController {
     @IBOutlet var operationSaveButton: UIButton!
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var desciptionTextField: UITextField!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
     
     var editingMode: Bool = false {
         didSet {
@@ -53,6 +54,7 @@ class ProfileViewController: UIViewController {
         self.operationSaveButton.layer.cornerRadius = 10
         self.pickPhotoButton.layer.masksToBounds = true
         self.profilePhotoImageView.layer.masksToBounds = true
+        self.activityIndicator.center = self.view.center
         
         imagePicker.delegate = self
         
@@ -69,7 +71,6 @@ class ProfileViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        // adding keyboard observers
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
@@ -77,26 +78,8 @@ class ProfileViewController: UIViewController {
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         
-        // removing keyboard observers
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    @objc func keyboardWillShow(_ notification: NSNotification) {
-        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
-            let keyboardHeight = keyboardFrame.cgRectValue.height
-            self.view.frame.origin.y = -keyboardHeight
-            print("keyboard height is:" , keyboardHeight)
-        }
-    }
-    
-    @objc func keyboardWillHide(sender: NSNotification) {
-        self.view.frame.origin.y = 0
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // hides keyboard when tapped outside keyboard
-        self.view.endEditing(true)
     }
     
     override func viewDidLayoutSubviews() {
@@ -135,6 +118,128 @@ class ProfileViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
+    private func loadFromFile() {
+        self.dataManager?.loadProfile(completion: { (profile) in
+            if let unwrappedProfile = profile {
+                self.profile = unwrappedProfile
+            }
+            
+            self.profilePhotoImageView.image = profile?.photo ?? UIImage.init(named: "placeholder-user")
+            self.nameTextField.text = profile?.name ?? "Введите имя"
+            self.desciptionTextField.text = profile?.description ?? "Расскажите о себе"
+            
+            self.profile = ProfileData(name: self.nameTextField.text, descr: self.desciptionTextField.text, image: self.profilePhotoImageView.image)
+        })
+    
+    }
+    
+    @IBAction func saveButtonsPressed(_ sender: UIButton) {
+        self.nameTextField.resignFirstResponder()
+        self.desciptionTextField.resignFirstResponder()
+        
+        self.saveChanges = {
+            
+            self.activityIndicator.startAnimating()
+            self.setEnabledState(enabled: false)
+            
+            self.profile?.name = self.nameTextField.text
+            self.profile?.description = self.desciptionTextField.text
+            self.profile?.photo = self.profilePhotoImageView.image
+            
+            let titleOfButton = sender.titleLabel?.text
+            
+            if titleOfButton == "Operation Save" {
+                self.dataManager = OperationDataManager()
+            } else {
+                self.dataManager = GCDDataManager()
+            }
+            
+            
+            self.dataManager?.saveProfile(data: self.profile!, completion: { (saveSucceeded : Bool) in
+                
+                self.activityIndicator.stopAnimating()
+                if saveSucceeded {
+                    self.showAlert(title: "Данные сохранены", message: "", retry: nil)
+                    self.loadFromFile()
+                } else {
+                    self.showAlert(title: "Ошибка", message: "Не удалось сохранить данные", retry: nil)
+                }
+                self.setEnabledState(enabled: true)
+                self.editingMode = !saveSucceeded
+            })
+        }
+        
+        self.saveChanges?();
+    }
+    
+    @IBAction func usernameChangedByEditing(_ sender: UITextField) {
+        if let newName = sender.text {
+            self.profile?.nameChanged = (newName != (self.profile?.name ?? ""))
+            self.setEnabledState(enabled: self.dataWasChanged)
+        }
+    }
+    
+    @IBAction func descrChangedByEditing(_ sender: UITextField) {
+        if let newDescr = sender.text {
+            self.profile?.descriptionChanged = (newDescr != (self.profile?.description ?? ""))
+            self.setEnabledState(enabled: self.dataWasChanged)
+        }
+    }
+    
+}
+
+// MARK: - Change ViewController State
+
+extension ProfileViewController {
+    
+    private func setEditingState(editing: Bool) {
+        if (editing) {
+            self.nameTextField.placeholder = "Введите имя"
+        }
+        
+        self.editButton.isHidden = editing
+        self.gcdSaveButton.isHidden = !editing
+        self.operationSaveButton.isHidden = !editing
+        
+        self.nameTextField.isEnabled = editing
+        self.desciptionTextField.isEnabled = editing
+    }
+    
+    
+    private func setEnabledState(enabled: Bool) {
+        self.gcdSaveButton.isEnabled = enabled
+        self.operationSaveButton.isEnabled = enabled
+    }
+    
+}
+
+// MARK: - Hide Keyboard
+
+extension ProfileViewController {
+    
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+            self.view.frame.origin.y = -keyboardHeight
+            print("keyboard height is:" , keyboardHeight)
+        }
+    }
+    
+    @objc func keyboardWillHide(sender: NSNotification) {
+        self.view.frame.origin.y = 0
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // hides keyboard when tapped outside keyboard
+        self.view.endEditing(true)
+    }
+    
+}
+
+// MARK: - Photo picker methods
+
+extension ProfileViewController {
+    
     func pickPhotoFromLibrary() {
         imagePicker.allowsEditing = false
         imagePicker.sourceType = .photoLibrary
@@ -160,95 +265,6 @@ class ProfileViewController: UIViewController {
         let okAction = UIAlertAction(title: "OK", style:.default, handler: nil)
         alertVC.addAction(okAction)
         present(alertVC, animated: true, completion: nil)
-    }
-    
-    private func loadFromFile() {
-        self.dataManager?.loadProfile(completion: { (profile) in
-            if let unwrappedProfile = profile {
-                self.profile = unwrappedProfile
-            }
-            
-            self.profilePhotoImageView.image = profile?.photo ?? UIImage.init(named: "placeholder-user")
-            self.nameTextField.text = profile?.name ?? "Введите имя"
-            self.desciptionTextField.text = profile?.description ?? "Расскажите о себе"
-            
-            self.profile = ProfileData(name: self.nameTextField.text, descr: self.desciptionTextField.text, image: self.profilePhotoImageView.image)
-        })
-    
-    }
-    
-    @IBAction func saveButtonsPressed(_ sender: UIButton) {
-        self.nameTextField.resignFirstResponder()
-        self.desciptionTextField.resignFirstResponder()
-        
-        self.saveChanges = {
-            
-            //self.activityIndicator.startAnimating()
-            self.setEnabledState(enabled: false)
-            
-            self.profile?.name = self.nameTextField.text
-            self.profile?.description = self.desciptionTextField.text
-            self.profile?.photo = self.profilePhotoImageView.image
-            
-            let titleOfButton = sender.titleLabel?.text
-            
-            if titleOfButton == "Operation Save" {
-                self.dataManager = OperationDataManager()
-            } else {
-                self.dataManager = GCDDataManager()
-            }
-            
-            
-            self.dataManager?.saveProfile(data: self.profile!, completion: { (saveSucceeded : Bool) in
-                
-                //self.activityIndicator.stopAnimating()
-                
-                if saveSucceeded {
-                    self.showAlert(title: "Данные сохранены", message: "", retry: nil)
-                    self.loadFromFile()
-                } else {
-                    self.showAlert(title: "Ошибка", message: "Не удалось сохранить данные", retry: nil)
-                }
-                
-                self.setEnabledState(enabled: true)
-                self.editingMode = !saveSucceeded
-            })
-        }
-        
-        self.saveChanges?();
-    }
-    
-    @IBAction func usernameChangedByEditing(_ sender: UITextField) {
-        if let newName = sender.text {
-            self.profile?.nameChanged = (newName != (self.profile?.name ?? ""))
-            self.setEnabledState(enabled: self.dataWasChanged)
-        }
-    }
-    
-    @IBAction func descrChangedByEditing(_ sender: UITextField) {
-        if let newDescr = sender.text {
-            self.profile?.descriptionChanged = (newDescr != (self.profile?.description ?? ""))
-            self.setEnabledState(enabled: self.dataWasChanged)
-        }
-    }
-    
-    private func setEditingState(editing: Bool) {
-        if (editing) {
-            self.nameTextField.placeholder = "Введите имя"
-        }
-        
-        self.editButton.isHidden = editing
-        self.gcdSaveButton.isHidden = !editing
-        self.operationSaveButton.isHidden = !editing
-        
-        self.nameTextField.isEnabled = editing
-        self.desciptionTextField.isEnabled = editing
-    }
-    
-    
-    private func setEnabledState(enabled: Bool) {
-        self.gcdSaveButton.isEnabled = enabled
-        self.operationSaveButton.isEnabled = enabled
     }
     
 }
@@ -289,7 +305,7 @@ extension ProfileViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.view.endEditing(true)
-        return true;
+        return true
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
